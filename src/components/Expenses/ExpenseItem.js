9882876edd1b96d81678b93219d2f170e90dced0
9Boxa4 <bogdanjovanovic10@gmail.com>
2022-03-15 17:53:18 +0100

    First commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import ExpenseDate from './ExpenseDate';
+import "./ExpenseItem.css";
+import Card from '../UI/Card';
+
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+//    const clickHandler = () =>
+//    {
+//        setTitle('Updated');
+//    };
+return(
+    <li>
+        <Card className='expense-item'>
+        <ExpenseDate date={props.date}/>
+            <div className='expsense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+            </div>
+            {/* <button >Change Title</button> */}
+        </Card>
+    </li>
+    )
+}
+
+  
+export default ExpenseItem
